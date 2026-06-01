062ab6385abb5ba4053a04466ae47ca94d4676f0
Alfonso Auquilla <alfonsoauquilla180@gmail.com>
2026-06-01 08:46:15 -0500

    controlers

---
+++ src/Utils/BenchMarking.java
@@ -0,0 +1,24 @@
+package Utils;
+import models.Resultado;
+import java.util.concurrent.Callable;
+public class BenchMarking {
+    public static Resultado medirTiempo(Callable<Void> funcion , String algoritmo, String escenario, int sample){
+    try {
+        long inicio = System.nanoTime();
+        funcion.call();
+        long fin = System.nanoTime();
+        double duracion = fin - inicio;
+
+        Resultado resultados = new Resultado(algoritmo,escenario, sample,duracion);
+        return resultados;
+        
+    } catch (Exception e) {
+
+        throw new RuntimeException("Error en la funcion");
+        
+    }
+
+    
+
+  }
+}
