66ffb698ef8b91989c33a966839bec9ce02d7f8e
Alfonso Auquilla <alfonsoauquilla180@gmail.com>
2026-06-01 09:02:07 -0500

    medir tiempo

--- src/App.java
+++ src/App.java
@@ -1,3 +1,7 @@
+import java.util.concurrent.Callable;
+
+import Utils.BenchMarking;
+import controles.SortPersonaMethods;
 import models.Persona;
 public class App {
     
@@ -12,5 +16,43 @@ public class App {
             }
             return personas;
         }
+
+       Persona[] base = generarPersonas(10000);
+       Persona[] copiaInsercion= base.clone();
+       Persona[] copiaQuickSort= base.clone();
+
+       SortPersonaMethods metodos = new SortPersonaMethods();
+        Callable<Void> funcionBubble = () ->{
+            metodos.insertionSort(copiaInsercion);            
+            return null;
+
+        };
+        Results rBubble = BenchMarking.medirTiempo(funcionBubble,"Burbuja: ",size);
+        System.out.println(rBubble.getName()+" = "+rBubble.getTime());
+
+        Callable<Void> funcionBubbleAvan = () ->{
+            metodos.sortBubbleAvan(arreglo3);             
+            return null;
+
+        };
+        Results rBubbleAvan = BenchMarking.medirTiempo(funcionBubbleAvan,"Burbuja avanzado: ",size);
+        System.out.println(rBubbleAvan.getName()+" = "+rBubbleAvan.getTime());
+
+        Callable<Void> funcionShell = () ->{
+            metodos.sortShell(arreglo3);             
+            return null;
+
+        };
+        Results rShell= BenchMarking.medirTiempo(funcionShell,"SHELL: ",size);
+        System.out.println(rShell.getName()+" = "+rShell.getTime());
+    }
+
+    public static int[] generate(int size){
+        int [] array = new int [size];
+        for(int i = 0; i < size;i ++){
+            int numero =(int)(Math.random()*10000);
+            array[i]=numero;
+        }
+        return array;
     
 }
